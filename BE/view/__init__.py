b11e248f974522119c128679ae6e8eb7a53fd72f
Hyoniii <phhphh841@hanmail.net>
2020-11-25 14:02:25 +0900

    Add : 초기세팅,blueprint

--- BE/view/__init__.py
+++ BE/view/__init__.py
@@ -1,9 +1,9 @@
 from .account_view import route_account
-from .product_view import route_product
+from .product_view import ProductView
 from .order_view   import route_order 
 
 __all__ = [
             'route_account',
-            'route_product',
+            'ProductView',
             'route_order'
 ]
