65c271b6683dc29f0aeaeb3fc7536b408f5a0628
김보현 <bohyunkim01@gmail.com>
2020-11-25 14:07:03 +0900

    회원가입 및 로그인 레이아웃 구현

--- FE/src/pages/login/login.vue
+++ FE/src/pages/login/login.vue
@@ -1,12 +1,121 @@
 <template>
-  <div>login</div>
+  <div id="login">
+    <div class="brandiLogo">
+      <img src="https://sadmin.brandi.co.kr/include/img/logo_seller_admin_1.png" alt="브랜디로고" />
+    </div>
+    <div class="loginContainer">
+      <section class="loginForm">
+        <div class="loginHeader">브랜디 어드민 로그인</div>
+        <div class="loginInputWrapper">
+          <a-input style="height: 50%" class="loginInfoInput" placeholder="아이디" />
+          <a-input style="height: 50%" class="loginInfoInput" placeholder="비밀번호" />
+        </div>
+      </section>
+
+      <section class="loginSubmit">
+        <div class="loginBtnWrapper">
+          <a-button style="height: 100%" class="loginBtn" v-on:click="handleSignupBtn">로그인</a-button>
+        </div>
+        <div class="registerOptions">
+          아직 셀러가 아니신가요?
+          <a class="registerStartBtn">회원가입하기</a>
+        </div>
+      </section>
+      <SellerList />
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
+  data() {
+    return {
+      idValue: "",
+      pwValue: "",
+    };
+  },
 
 }
 </script>
 
-<style>
+<style lang="scss">
+@import "../../styles.scss";
+
+#login {
+  height: 100vh;
+  margin: 0 auto;
+  padding: 65px 0 50px;
+  background-color: $default-background;
+
+  .brandiLogo {
+    display: block;
+    margin-bottom: 40px;
+    text-align: center;
+
+    img {
+      width: 130px;
+      max-width: 100%;
+      vertical-align: top;
+    }
+  }
+
+  .loginContainer {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    width: 380px;
+    height: 470px;
+    padding: 64px 35px 64px 35px;
+    margin: 0 auto;
+    background-color: $default-white;
+    border-radius: 20px;
+    box-shadow: 0 4px 31px 0 rgba(0, 0, 0, 0.1);
+
+    .loginForm {
+      display: flex;
+      flex-direction: column;
+
+      .loginHeader {
+        margin: 0 0 25px 0;
+        font-size: 24px;
+        font-weight: 700;
+        text-align: left;
+        line-height: 1.5;
+        letter-spacing: -1.5px;
+        text-indent: 2px;
+        color: $black;
+      }
+
+      .loginInputWrapper {
+        margin-bottom: 15px;
+
+        .loginInfoInput {
+          margin-bottom: 10px;
+          border-radius: 8px;
+        }
+      }
+    }
+    .loginSubmit {
+      .loginBtnWrapper {
+        height: 100%;
+        .loginBtn {
+          background-color: $black;
+          color: $default-white;
+          width: 100%;
+          font-size: 12px;
+          border-radius: 8px;
+        }
+      }
+      .registerOptions {
+        padding-top: 10px;
+        font-size: 12px;
+        text-align: center;
+
+        .registerStartBtn {
+          font-size: 12px;
+        }
+      }
+    }
+  }
+}
 </style>
--- FE/src/pages/signup/signup.vue
+++ FE/src/pages/signup/signup.vue
@@ -1,16 +1,20 @@
 <template>
-  <div class="signUp">
+  <div id="signup">
     <div class="signupContainer">
       <div class="brandiLogo">
         <img alt="로고이미지" src="https://sadmin.brandi.co.kr/include/img/logo_seller_admin_1.png" />
       </div>
       <div class="signupHeader">
-        <div class="signupHeading">셀러회원 가입</div>
+        <div class="signupHeading">회원가입</div>
         <div class="divider"></div>
-
-        <div class="signupFlow">
-          <div class="signupFlowBanner">정보입력</div>
-        </div>
+        <a-radio-group class="signupAccountTypes" v-model="accountType" button-style="solid">
+          <a-radio-button
+            :value="signupAccountType.id"
+            :key="signupAccountType.id"
+            v-for="signupAccountType in signupAccountTypes"
+            class="signupAccountType"
+          >{{ signupAccountType.value }}</a-radio-button>
+        </a-radio-group>
       </div>
       <div class="signupInfo">
         <section class="basicSellerInfo signupSection">
@@ -19,19 +23,21 @@
             <a-input class="inputDefault" placeholder="아이디" v-model="idValue">
               <a-icon slot="prefix" type="user" />
             </a-input>
-            <a-input class="inputDefault" placeholder="비밀번호" v-model="pwValue">
+            <p v-if="errorMessage" class="[errorMessage ? errorMessage : '']">{{ idErrorMessage }}</p>
+            <a-input-password class="inputDefault" placeholder="비밀번호" v-model="pwValue">
               <a-icon slot="prefix" type="lock" theme="filled" />
-            </a-input>
-            <a-input
+            </a-input-password>
+            <p v-if="errorMessage" class="[errorMessage ? errorMessage : '']">{{ pwErrorMessage }}</p>
+            <a-input-password
               class="inputDefault inputDefaultLastChild"
               placeholder="비밀번호 재입력"
               v-model="pwConfirmValue"
             >
               <a-icon slot="prefix" type="check" />
-            </a-input>
+            </a-input-password>
           </div>
         </section>
-        <section class="managerInfo signupSection">
+        <section v-show="sellerAccountType" class="managerInfo signupSection">
           <div class="sectionTitle">
             담당자 정보
             <span class="titleHelper">(*실제 샵을 운영하시는 분)</span>
@@ -45,58 +51,78 @@
               <a-icon slot="prefix" type="phone" theme="filled" />
             </a-input>
             <p
+              v-if="errorMessage"
+              class="[errorMessage ? errorMessage : '']"
+            >{{ phoneErrorMessage }}</p>
+            <p
               class="managerInstructions inputDefaultLastChild"
             >입점 신청 후 브랜디 담당자가 연락을 드릴 수 있으니 정확한 정보를 기입해주세요.</p>
           </div>
         </section>
-        <section class="signupSection sellerInfo">
+        <section v-show="!sellerAccountType" class="signupSection sellerInfo">
+          <div class="sectionTitle">마스터 정보</div>
+          <div class="sellerInputs">
+            <a-input class="inputDefault" placeholder="마스터 시크릿코드" v-model="masterSecretCode">
+              <a-icon slot="prefix" type="question" />
+            </a-input>
+            <p
+              v-if="errorMessage"
+              class="[errorMessage ? errorMessage : '']"
+            >{{ secretcodeErrorMessage }}</p>
+          </div>
+        </section>
+        <section v-show="sellerAccountType" class="signupSection sellerInfo">
           <div class="sectionTitle">셀러 정보</div>
-
           <div class="sellerTypes">
-            <a-radio-group v-model="sellerType" @change="onChange">
-              <label class="sellerTypesOptions">
-                <a-radio value="쇼핑몰">쇼핑몰</a-radio>
-              </label>
-              <label class="sellerTypesOptions">
-                <a-radio value="마켓">마켓</a-radio>
-              </label>
-              <label class="sellerTypesOptions">
-                <a-radio value="로드샵">로드샵</a-radio>
-              </label>
-              <label class="sellerTypesOptions">
-                <a-radio value="디자이너 브랜드">디자이너 브랜드</a-radio>
-              </label>
-              <label class="sellerTypesOptions">
-                <a-radio value="제너럴 브랜드">제너럴 브랜드</a-radio>
-              </label>
-              <label class="sellerTypesOptions">
-                <a-radio value="내셔널 브랜드">내셔널 브랜드</a-radio>
-              </label>
-              <label class="sellerTypesOptions">
-                <a-radio value="뷰티">뷰티</a-radio>
-              </label>
+            <a-radio-group v-model="sellerType">
+              <a-radio
+                :value="sellerTypeOption.id"
+                :key="sellerTypeOption.id"
+                v-for="sellerTypeOption in sellerTypeOptions"
+                class="sellerTypesOptions"
+              >{{ sellerTypeOption.value }}</a-radio>
             </a-radio-group>
           </div>
           <div class="sellerInputs">
             <a-input class="inputDefault" placeholder="셀러명 (상호)" v-model="sellerNameValue">
               <a-icon slot="prefix" type="smile" />
             </a-input>
+            <p
+              v-if="errorMessage"
+              class="[errorMessage ? errorMessage : '']"
+            >{{ sellerNameErrorMessage }}</p>
             <a-input
-              class="inputDefault inputDefaultLastChild"
+              class="inputDefault"
               placeholder="영문 셀러명 (영문상호)"
               v-model="sellerEnglishNameValue"
             >
               <a-icon slot="prefix" type="star" theme="filled" />
             </a-input>
+            <p
+              v-if="errorMessage"
+              class="[errorMessage ? errorMessage : '']"
+            >{{ sellerEnglishNameErrorMessage }}</p>
+
+            <a-input
+              class="inputDefault inputDefaultLastChild"
+              placeholder="고객센터 전화번호"
+              v-model="customerServiceNumber"
+            >
+              <a-icon slot="prefix" type="phone" theme="filled" />
+            </a-input>
+            <p
+              v-if="errorMessage"
+              class="[errorMessage ? errorMessage : '']"
+            >{{ customerServiceNumberErrorMessage }}</p>
           </div>
         </section>
-      </div>
-      <div class="signupBtnsContainer">
-        <div class="signupBtns">
-          <a-button-group class="signupBtnsGroup">
-            <a-button class="btnGrouped" type="primary">fd</a-button>
-            <a-button class="btnGrouped" type="danger">취소</a-button>
-          </a-button-group>
+        <div class="signupBtnsContainer">
+          <div class="signupBtns">
+            <a-button-group class="signupBtnsGroup">
+              <a-button class="btnGrouped" type="danger" v-on:click="handleCancelBtn">취소</a-button>
+              <a-button class="btnGrouped" type="primary">신청</a-button>
+            </a-button-group>
+          </div>
         </div>
       </div>
     </div>
@@ -107,28 +133,91 @@
 export default {
   data() {
     return {
+      sellerAccountType: true,
       idValue: "",
       pwValue: "",
       pwConfirmValue: "",
       managerNumberValue: "",
       sellerNameValue: "",
       sellerEnglishNameValue: "",
-      sellerType: ""
+      customerServiceNumber: "",
+      sellerType: "쇼핑몰",
+      accountType: 1,
+      masterSecretCode: "",
+      hasError: false,
+      idErrorMessage: "",
+      pwErrorMessage: "",
+      phoneErrorMessage: "",
+      secretcodeErrorMessage: "",
+      sellerNameErrorMessage: "",
+      sellerEnglishNameErrorMessage: "",
+      customerServiceNumberErrorMessage: "",
+      sellerTypeOptions: [
+        { id: 1, value: "쇼핑몰"},
+        { id: 2, value: "마켓"},
+        { id: 3, value: "로드샵"},
+        { id: 4, value: "디자이너 브랜드"},
+        { id: 5, value: "제너럴 브랜드"},
+        { id: 6, value: "내셔널 브랜드"},
+        { id: 7, value: "뷰티"},
+        ],
+      signupAccountTypes: [
+        { id: 1, value: "셀러 가입"},
+        { id: 2, value: "마스터 가입"},
+        ],
     };
-  }
+  },
+
+  methods: {
+  handleCancelBtn() {
+    alert("브랜디 가입을 취소하시겠습니까?")
+  },
+  handleRegisterBtn() {
+const validatePw = RegExp(/^(?=.*[A-Za-z])(?=.*[0-9])(?=.*[~!@#$%^&*()\-_=+,.<>/?;:'"[{}\]\\|]).{8,20}$/);
+let isValidPw = validatePw.test(this.pwValue);
+
+const validateId = RegExp(/^([A-Za-z0-9\uac00-\ud7af])+(\1?)$/);
+let isValidId = validateId.test(this.idValue);
+
+const validateCustomerService = RegExp(/^([\d-]+)$/);
+let isValidCustomerServiceNumber = validateCustomerServiceNumber.test(this.customerServiceNumber);
+
+if (this.idValue.length < 1){
+  idErrorMessage=""
+}
+
+  },
+
+  //   if (idValue.length < 2  )
+    
+  //   } else if (idValue === /^([A-Za-z0-9])+([A-Za-z0-9_-]){4,19}$/){
+  //     return errorMessage="아이디는 5-~20글자의 영문, 숫자, 언더바, 하이픈만 사용 가능하며 시작 문자는 영문 또는 숫자입니다."
+  //   }
+  //   if ()
+  // },
+
+  },
+  
+  computed: {
+
+  },
+  watch:{
+    accountType(val){
+      return val===1 ? this.sellerAccountType=true : this.sellerAccountType=false;
+    }
+  },
 };
 </script>
 
 <style lang="scss">
 @import "../../styles.scss";
 
-.signUp {
-  height: 100vh;
+#signup {
   background-color: $default-background;
 
   .signupContainer {
     width: 500px;
-    height: 100%;
+    height: 100vh;
     margin: 0 auto;
     margin-bottom: 0px;
     padding: 20px 30px 15px 30px;
@@ -138,6 +227,7 @@ export default {
 
     .brandiLogo {
       margin-bottom: 40px;
+      text-align: center;
 
       img {
         width: 130px;
@@ -152,10 +242,11 @@ export default {
       justify-content: space-around;
       width: 100%;
       text-align: center;
+      padding: 0px 15px;
 
       .signupHeading {
         display: block;
-        margin-top: 20px;
+        margin-top: 10px;
         // margin-bottom: 10px;
         font-size: 24px;
         line-height: 1.1;
@@ -166,17 +257,9 @@ export default {
         border-bottom: $default-border;
       }
 
-      .signupFlow {
-        display: block;
-        margin: 0 15px;
-
-        .signupFlowBanner {
-          margin: 0 auto;
-          padding: 10px 0;
-          color: $default-grey;
-          background-color: $default-black;
-          font-size: 18px;
-          text-align: center;
+      .signupAccountTypes {
+        .signupAccountType {
+          width: 50%;
         }
       }
     }
@@ -213,11 +296,11 @@ export default {
       }
       .sellerInfo {
         .sellerTypes {
-          margin: 10px 10px;
+          display: flex;
+          margin: 15px 10px;
 
           .sellerTypesOptions {
-            display: inline-block;
-            margin-right: 12px;
+            margin-right: 10px;
             input {
             }
           }
@@ -227,14 +310,16 @@ export default {
           }
         }
       }
-    }
+      .signupBtnsContainer {
+        margin: 30px 0;
+        text-align: center;
 
-    .signupBtnsContainer {
-      .signupBtns {
-        .signupBtnsGroup {
-          background-color: transparent;
-          .btnGrouped {
-            color: $default-white;
+        .signupBtns {
+          .signupBtnsGroup {
+            background-color: transparent;
+            .btnGrouped {
+              color: $default-white;
+            }
           }
         }
       }
