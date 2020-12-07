f70f80540da8bb010d0f5663565e0793ab1d2067
김보현 <bohyunkim01@gmail.com>
2020-12-07 13:51:45 +0900

    Modify: 셀러관리 MockData 포맷 변경

--- FE/src/main.js
+++ FE/src/main.js
@@ -5,10 +5,13 @@ import Routes from "./Routes";
 import "ant-design-vue/dist/antd.css";
 import Antd from "ant-design-vue";
 import "../src/styles.scss";
-import axios from 'axios'
+import axios from "axios";
 
 Vue.use(Antd);
 Vue.use(VueRouter);
+
+axios.defaults.headers.common["AUTHORIZATION"] =
+  "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJhY2NvdW50X2lkIjoxMjcsImV4cGlyYXRpb24iOiIyMDIwLTEyLTAyIDA2OjA5OjIwLjIzNTU0OCJ9.wug1dOSu9ZjcQXN5xwp35kAtr-N1wrDX0P7D7d2r9bo";
 
 const router = new VueRouter({
   routes: Routes,
