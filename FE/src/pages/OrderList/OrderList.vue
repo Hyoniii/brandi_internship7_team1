bec364f0d8cc3ead37cef8473c069bc541975017
김보현 <bohyunkim01@gmail.com>
2020-12-07 14:21:30 +0900

    제품관리 및 주문관리 폴더 생성

---
+++ FE/src/pages/OrderList/OrderList.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="OrderList">ORDER LIST FOR SOJEONGGG</div>
+</template>
+
+<script>
+export default {
+  name: "orderlist",
+  components: {},
+  data() {},
+  method: {},
+  computed: {},
+  mounted() {},
+};
+</script>
+
+<style lang="css">
+@import "../../styles.scss";
+</style>
---
+++ FE/src/pages/ProductList/ProductList.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="ProductList">PRODUCT LIST FOR HYONNIII</div>
+</template>
+
+<script>
+export default {
+  name: "productlist",
+  components: {
+  },
+  data() {},
+  method: {},
+  computed: {},
+  mounted() {},
+};
+</script>
+
+<style lang="css">
+@import "../../styles.scss";
+</style>
