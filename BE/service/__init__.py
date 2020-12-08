d1aeb9953e7bca29e7cbeefd98d9f56f629f960e
sojeong20 <sojeong.heo@gmail.com>
2020-12-08 23:18:49 +0900

    Add: seller home 엔드포인트

--- BE/service/__init__.py
+++ BE/service/__init__.py
@@ -1,9 +1,11 @@
-# from .account_service import AccountService
+from .account_service import AccountService
 from .product_service import ProductService
 from .order_service   import OrderService
+from .seller_service  import SellerService
 
 __all__ = [
-            #'AccountService',
+            'AccountService',
             'ProductService',
-            'OrderService'
+            'OrderService',
+            'SellerService',
 ]
--- BE/view/__init__.py
+++ BE/view/__init__.py
@@ -1,8 +1,10 @@
 from .account_view import AccountView
 from .product_view import ProductView
-from .order_view import OrderView
+from .order_view   import OrderView
+from .seller_view  import SellerView
 __all__ = [
             'AccountView',
             'ProductView',
-            'OrderView'
+            'OrderView',
+            'SellerView',
 ]
